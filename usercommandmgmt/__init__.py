52a4baad611b7c38d46f7b03955db97f4936a557
Jacob Lacsamana <JLacsamana@bgcengineering.ca>
2023-10-08 17:54:41 -0700

    awaited bot loading

--- usercommandmgmt/__init__.py
+++ usercommandmgmt/__init__.py
@@ -2,5 +2,5 @@ from .usercommandmgmt import Usercommandmgmt
 from redbot.core.bot import Red
 
 
-def setup(bot: Red):
-    bot.add_cog(Usercommandmgmt(bot))
+async def setup(bot: Red):
+    await bot.add_cog(Usercommandmgmt(bot))
